75ed2f4074efd2c9a35a11038e2e319e7e15169c
Karthik Anant <karthika25@gmail.com>
2016-11-25 00:32:28 -0500

    added a badge tag helper

---
+++ src/TagSharp/Bootstrap/BadgeTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagSharp.Bootstrap
+{
+    [HtmlTargetElement("ts-bootstrap-badge")]
+    public class BadgeTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var awaiter = await output.GetChildContentAsync();
+            var content = awaiter.GetContent();
+            var html = string.Format(@"<span class=""badge"">{0}</span>", content);
+
+            output.TagName = "";
+            output.Content.AppendHtml(html);
+        }
+    }
+}
