57b481cdbaa3c46d41920f7eb4d4ab1ecb761a7b
Karthik Anant <karthika25@gmail.com>
2016-11-24 21:55:04 -0500

    added a alert tag helper

--- TagSharpSample/Controllers/HomeController.cs
+++ TagSharpSample/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace TagSharpSample.Controllers
 {
@@ -16,13 +12,6 @@ namespace TagSharpSample.Controllers
         public IActionResult About()
         {
             ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
 
             return View();
         }
---
+++ src/TagSharp/Bootstrap/AlertTagHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace TagSharp.Bootstrap
+{
+    [HtmlTargetElement("ts-bootstrap-alert")]
+    public class AlertTagHelper : TagHelper
+    {
+        private const string CssClassAttributeName = "bs-css-class";
+
+        [HtmlAttributeName(CssClassAttributeName)]
+        public string CssClass { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var childContentAwaiter = await output.GetChildContentAsync();
+            var childContent = childContentAwaiter.GetContent();
+
+            var template = @"<div class=""alert {0}"" role=""alert"">
+                                {1}
+                             </div>";
+            var cssClass = !string.IsNullOrEmpty(CssClass) ? CssClass : "alert-success";
+
+            output.Content.AppendHtml(string.Format(template, cssClass, childContent));
+        }
+    }
+}
