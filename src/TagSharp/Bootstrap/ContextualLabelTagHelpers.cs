44359a499b268714a0639deaa7b98e59b391a79e
Karthik Anant <karthika25@gmail.com>
2016-11-25 21:39:40 -0500

    added the label tag helper

---
+++ src/TagSharp/Bootstrap/ContextualLabelTagHelpers.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace TagSharp.Bootstrap
+{
+    [HtmlTargetElement("ts-bootstrap-label-success")]
+    public class LabelSuccessTagHelper : LabelTagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            this.CssClass = "label-success";
+            return base.ProcessAsync(context, output);
+        }
+    }
+
+    [HtmlTargetElement("ts-bootstrap-label-primary")]
+    public class LabelPrimaryTagHelper : LabelTagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            this.CssClass = "label-primary";
+            return base.ProcessAsync(context, output);
+        }
+    }
+
+    [HtmlTargetElement("ts-bootstrap-label-info")]
+    public class LabelInfoTagHelper : LabelTagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            this.CssClass = "label-info";
+            return base.ProcessAsync(context, output);
+        }
+    }
+
+    [HtmlTargetElement("ts-bootstrap-label-warning")]
+    public class LabelWarningTagHelper : LabelTagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            this.CssClass = "label-warning";
+            return base.ProcessAsync(context, output);
+        }
+    }
+
+    [HtmlTargetElement("ts-bootstrap-label-danger")]
+    public class LabelDangerTagHelper : LabelTagHelper
+    {
+        public override Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            this.CssClass = "label-danger";
+            return base.ProcessAsync(context, output);
+        }
+    }
+}
---
+++ src/TagSharp/Bootstrap/LabelTagHelper.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Threading.Tasks;
+
+namespace TagSharp.Bootstrap
+{
+    [HtmlTargetElement("ts-bootstrap-label")]
+    public class LabelTagHelper : TagHelper
+    {
+        private const string CssClassAttributeName = "bs-css-class";
+
+        [HtmlAttributeName(CssClassAttributeName)]
+        public string CssClass { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var template = @"<span class=""{0}"">{1}</span>";
+            var childContentAwaiter = await output.GetChildContentAsync();
+            var childContent = childContentAwaiter.GetContent();
+
+            var cssClass = !string.IsNullOrEmpty(CssClass) ? string.Format("label {0}", CssClass) : "label label-default";
+
+            output.TagName = "";
+            output.Content.AppendHtml(string.Format(template, cssClass, childContent));
+        }
+    }
+}
