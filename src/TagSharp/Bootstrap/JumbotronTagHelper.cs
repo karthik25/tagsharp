0f4afcac16b044bfe8e8dcc45b4444fdf3cc4025
Karthik Anant <karthika25@gmail.com>
2016-11-25 21:31:54 -0500

    added the jumbotron tag helper

---
+++ src/TagSharp/Bootstrap/JumbotronTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagSharp.Bootstrap
+{
+    [HtmlTargetElement("ts-bootstrap-jumbotron")]
+    public class JumbotronTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var awaiter = await output.GetChildContentAsync();
+            var content = awaiter.GetContent();
+
+            var template = @"<div class=""jumbotron"">{0}</div>";
+
+            output.TagName = "";
+            output.Content.AppendHtml(string.Format(template, content));
+        }
+    }
+}
