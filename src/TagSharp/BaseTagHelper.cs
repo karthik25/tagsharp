112991aab883b8fa5fd6813a20d85c405ff20e81
Karthik Anant <karthika25@gmail.com>
2016-11-28 23:24:18 -0500

    even more refactoring for the base tag helper

--- src/TagSharp/BaseTagHelper.cs
+++ src/TagSharp/BaseTagHelper.cs
@@ -6,20 +6,18 @@ namespace TagSharp
 {
     public class BaseTagHelper : TagHelper
     {
-        public async Task<string> GetContentAsync(TagHelperContext context, TagHelperOutput output)
+        public async Task<string> GetContentAsync(TagHelperContext context, 
+                                                  TagHelperOutput output)
         {
             var awaiter = await output.GetChildContentAsync();
             var content = awaiter.GetContent();
             return content;
         }
-
-        public async Task<string> GetContentAsync(TagHelperContext context, TagHelperOutput output, string template)
-        {
-            var content = await GetContentAsync(context, output, template, parameters: new string[] { });
-            return content;
-        }
-
-        public async Task<string> GetContentAsync(TagHelperContext context, TagHelperOutput output, string template, params string[] parameters)
+        
+        public async Task<string> GetContentAsync(TagHelperContext context, 
+                                                  TagHelperOutput output, 
+                                                  string template, 
+                                                  params string[] parameters)
         {
             var content = await GetContentAsync(context, output);
             var values = new[] { content }.Concat(parameters).ToArray();
